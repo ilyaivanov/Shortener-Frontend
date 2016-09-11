24dfa979218335a30c014d8519cfc7657f620d29
Ilya Ivanov <static.ila@gmail.com>
2016-09-11 12:26:05 +0300

    Add shorening results

--- src/App.js
+++ src/App.js
@@ -4,25 +4,36 @@ import Shorten from './components/ShortenPage';
 export default class App extends React.Component {
     //statefull container
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.setUrl = this.setUrl.bind(this);
         this.shorten = this.shorten.bind(this);
+
+        //set initial state, otherwise null
+        this.state = {
+            url: ""
+        }
     }
 
-    setUrl(url){
+    setUrl(url) {
         this.setState({url});
     }
 
-    shorten(){
-        console.log('shortening', this.state.url);
+    shorten() {
+        var shortened = {
+            longUrl: this.state.url,
+            shortUrl: 'http://www.google.com',
+        };
+        this.setState({shortened});
     }
 
     render() {
         return (
             <div>
-                <Shorten onUrlChange={this.setUrl} shorten={this.shorten}/>
+                <Shorten onUrlChange={this.setUrl}
+                         shorten={this.shorten}
+                         shortened={this.state.shortened}/>
             </div>
         );
     }
--- src/components/ShortenPage.js
+++ src/components/ShortenPage.js
@@ -1,12 +1,15 @@
 import React, {PropTypes} from 'react';
-import {Form, FormControl, ControlLabel, FormGroup, Button} from 'react-bootstrap';
+import {Well, Collapse, Form, FormControl, ControlLabel, FormGroup, Button} from 'react-bootstrap';
+let shorten = props => {
 
-let shorten = props =>
-    (<div className="container">
+    var link = props.shortened ? props.shortened.shortUrl : "";
+    return (<div className="container">
         <h1>Shortener</h1>
-
+        <Collapse in={!!props.shortened}>
+            <div><a href={link}>{link}</a> <Button bsSize="xsmall">copy</Button></div>
+        </Collapse>
         <Form>
-            <FormGroup controlId="formInlineName">
+            <FormGroup>
                 <ControlLabel>Url</ControlLabel>
                 <FormControl onChange={e => props.onUrlChange(e.target.value)}
                              type="text"
@@ -17,10 +20,11 @@ let shorten = props =>
             </Button>
         </Form>
     </div>);
-
+}
 shorten.propTypes = {
     shorten: PropTypes.func.isRequired,
-    onUrlChange: PropTypes.func.isRequired
+    onUrlChange: PropTypes.func.isRequired,
+    shortened: PropTypes.object
 };
 
 export default shorten;
