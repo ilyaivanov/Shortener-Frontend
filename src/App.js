2b4b9e161e81c4c85f3f78423fe9dc2863fcacd2
Ilya Ivanov <static.ila@gmail.com>
2016-09-11 12:04:58 +0300

    Create basic events for changing url and shortening

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React, {PropTypes} from 'react';
 import Shorten from './components/ShortenPage';
 
 export default class App extends React.Component {
-
     //statefull container
+
+    constructor(props){
+        super(props);
+
+        this.setUrl = this.setUrl.bind(this);
+        this.shorten = this.shorten.bind(this);
+    }
+
+    setUrl(url){
+        this.setState({url});
+    }
+
+    shorten(){
+        console.log('shortening', this.state.url);
+    }
 
     render() {
         return (
             <div>
-                <Shorten />
+                <Shorten onUrlChange={this.setUrl} shorten={this.shorten}/>
             </div>
         );
     }
 }
-
-App.propTypes = {
-    children: PropTypes.element
-};
-
--- src/components/ShortenPage.js
+++ src/components/ShortenPage.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import {Form, FormControl, ControlLabel, FormGroup, Button} from 'react-bootstrap';
 
-export default () =>
+let shorten = props =>
     (<div className="container">
-        <h1>Shorten</h1>
+        <h1>Shortener</h1>
+
         <Form>
             <FormGroup controlId="formInlineName">
                 <ControlLabel>Url</ControlLabel>
-                {' '}
-                <FormControl type="text" placeholder="https://example.com/very/long/link/indeed"/>
+                <FormControl onChange={e => props.onUrlChange(e.target.value)}
+                             type="text"
+                             placeholder="https://example.com/very/long/link/indeed"/>
             </FormGroup>
-            <Button type="submit">
+            <Button onClick={props.shorten}>
                 Shorten
             </Button>
         </Form>
     </div>);
+
+shorten.propTypes = {
+    shorten: PropTypes.func.isRequired,
+    onUrlChange: PropTypes.func.isRequired
+};
+
+export default shorten;
 
