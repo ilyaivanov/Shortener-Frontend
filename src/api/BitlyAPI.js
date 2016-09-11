236a203fcf8224fc9668befb4f12587f564e7340
Ilya Ivanov <static.ila@gmail.com>
2016-09-11 13:02:02 +0300

    Add url encoding

--- src/api/BitlyAPI.js
+++ src/api/BitlyAPI.js
@@ -3,9 +3,9 @@ import $ from 'jquery';
 export default class BitlyA {
     shorten(url) {
         return new Promise(function (resolve, reject) {
-
+            var encodedUrl = encodeURIComponent(url);
             let accessToken = 'a06400a1dcadee2de02a7df7bee8110fb03523ee';
-            let bitLyrequest = `https://api-ssl.bitly.com/v3/shorten?access_token=${accessToken}&longUrl=${url}`;
+            let bitLyrequest = `https://api-ssl.bitly.com/v3/shorten?access_token=${accessToken}&longUrl=${encodedUrl}`;
 
             let bitLyResponse = {
                 "status_code": 200,
