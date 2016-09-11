c0e8ffdad54faa442b5587472d4b80ab12ecfa77
Ilya Ivanov <static.ila@gmail.com>
2016-09-11 11:52:50 +0300

    Remove route. Declare statefull component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, {PropTypes} from 'react';
-import {Link, IndexLink} from 'react-router';
-
+import Shorten from './components/ShortenPage';
 
 export default class App extends React.Component {
+
+    //statefull container
+
     render() {
         return (
             <div>
-                <IndexLink to="/">Search</IndexLink>
-                {' | '}
-                <Link to="/about">About</Link>
-                <br/>
-                {this.props.children}
+                <Shorten />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,25 +2,11 @@
 
 import React from 'react';
 import {render} from 'react-dom';
-import { Router, browserHistory, Route, IndexRoute} from 'react-router';
 import App from './App';
-import SearchPage from './components/ShortenPage';
-import AboutPage from './components/AboutPage';
-import NotFoundPage from './components/NotFoundPage.js';
 
 require('./favicon.ico');
 import './styles/styles.scss';
 
 import './../node_modules/bootstrap/dist/css/bootstrap.css';
 
-var routes = <Route path="/" component={App}>
-    <IndexRoute component={SearchPage}/>
-    <Route path="about" component={AboutPage}/>
-    <Route path="*" component={NotFoundPage}/>
-</Route>;
-
-render(
-    <Router history={browserHistory} routes={routes} />
-    ,
-    document.getElementById('app')
-);
+render(<App/>, document.getElementById('app'));
