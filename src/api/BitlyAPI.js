6ec0580028c7a76dab477e4fe5775a65e12e9ec7
Ilya Ivanov <static.ila@gmail.com>
2016-09-11 16:21:30 +0300

    Add additional time delay to test long requests

--- src/api/BitlyAPI.js
+++ src/api/BitlyAPI.js
@@ -2,33 +2,24 @@ import $ from 'jquery';
 
 export default class BitlyA {
     shorten(url) {
+        //set this flag to debug user experience when bit.ly is responsing to long
+        var additionalDelay = 0;
+
         return new Promise(function (resolve, reject) {
             let encodedUrl = encodeURIComponent(url);
             let accessToken = 'a06400a1dcadee2de02a7df7bee8110fb03523ee';
             let bitLyrequest = `https://api-ssl.bitly.com/v3/shorten?access_token=${accessToken}&longUrl=${encodedUrl}`;
 
-            // bitLySampleResponse = {
-            //     "status_code": 200,
-            //     "status_txt": "OK",
-            //     "data": {
-            //         "long_url": "original response",
-            //         "url": "http:\/\/bit.ly\/2ciW6PZ",
-            //         "hash": "2ciW6PZ",
-            //         "global_hash": "2ciV2v8",
-            //         "new_hash": 1
-            //     }
-            // }
-
             $.ajax(bitLyrequest)
                 .done(function (response) {
                     if (response.status_code == 200) {
-                        resolve(response.data)
+                        setTimeout(() => resolve(response.data), additionalDelay);
                     } else {
-                        var humanMessages = {
+                        var errorCodes = {
                             "INVALID_URI": "Invalid Url",
                             "ALREADY_A_BITLY_LINK": "You are trying to short a bit.ly link"
                         };
-                        var message = humanMessages[response.status_txt] || 'Unknown error';
+                        var message = errorCodes[response.status_txt] || 'Unknown error';
                         reject(message);
                     }
                 })
@@ -36,3 +27,16 @@ export default class BitlyA {
         });
     }
 }
+
+// bitLySampleResponse = {
+//     "status_code": 200,
+//     "status_txt": "OK",
+//     "data": {
+//         "long_url": "original response",
+//         "url": "http:\/\/bit.ly\/2ciW6PZ",
+//         "hash": "2ciW6PZ",
+//         "global_hash": "2ciV2v8",
+//         "new_hash": 1
+//     }
+// }
+
