f80f2ededdae98837452611fdda183c71159a887
Ilya Ivanov <static.ila@gmail.com>
2016-09-11 18:53:26 +0300

    Missing behaviour for bit.ly endpoint

--- src/api/BitlyAPI.js
+++ src/api/BitlyAPI.js
@@ -5,26 +5,34 @@ export default class BitlyA {
         //set this flag to debug user experience when bit.ly is responsing to long
         var additionalDelay = 0;
 
-        return new Promise(function (resolve, reject) {
+        return new Promise((resolve, reject) => {
             let encodedUrl = encodeURIComponent(url);
             let accessToken = 'a06400a1dcadee2de02a7df7bee8110fb03523ee';
             let bitLyrequest = `https://api-ssl.bitly.com/v3/shorten?access_token=${accessToken}&longUrl=${encodedUrl}`;
 
-            $.ajax(bitLyrequest)
-                .done(function (response) {
-                    if (response.status_code == 200) {
-                        setTimeout(() => resolve(response.data), additionalDelay);
-                    } else {
-                        var errorCodes = {
-                            "INVALID_URI": "Invalid Url",
-                            "ALREADY_A_BITLY_LINK": "You are trying to short a bit.ly link"
-                        };
-                        var message = errorCodes[response.status_txt] || 'Unknown error';
-                        reject(message);
-                    }
-                })
-                .fail(reject);
+            $
+            .ajax(bitLyrequest)
+            .done(response => {
+                if (response.status_code == 200) {
+                    setTimeout(() => resolve(this.mapResponse(response.data)), additionalDelay);
+                } else {
+                    var errorCodes = {
+                        "INVALID_URI": "Invalid Url",
+                        "ALREADY_A_BITLY_LINK": "You are trying to short a bit.ly link"
+                    };
+                    var message = errorCodes[response.status_txt] || 'Unknown error';
+                    reject(message);
+                }
+            })
+            .fail(reject);
         });
+    }
+
+    mapResponse(data) {
+        return {
+            shorten_url: data.url,
+            url: data.long_url
+        }
     }
 }
 
